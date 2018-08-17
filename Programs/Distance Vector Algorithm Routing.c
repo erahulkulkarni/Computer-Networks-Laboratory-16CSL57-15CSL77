6b9668b4acae19b4ec8ccf579881f200616e8a12
rahul <erahulkulkarni@gmail.com>
2018-08-17 21:42:59 +0530

    CRC

--- Programs/Distance Vector Algorithm Routing.c
+++ Programs/Distance Vector Algorithm Routing.c
@@ -1,10 +1,180 @@
-/* 
-	Computer Networks Laboratory (Lab) 15CSL77
-	7. Write a program for distance vector algorithm to find suitable path for transmission
-*/
+ /* 
+   Computer Networks Laboratory (Lab) 15CSL77
+   7. Write a program for distance vector algorithm to find suitable path for 
+        transmission
+ */
 
-/*
+ /* Network layer
+    Router, Routing
+    Routing Table
+    Single source shortest path
+    All pair shortest path
+    Distance vector
+ */
 
-*/
+ /* The Bellman-Ford algorithm solves the single-source shortest-paths problem in 
+      the general case in which edge weights may be negative.
+      Where as Dijkastra Algorithm fails in this case.
 
-/* Textbook: Behrouz Forouzon - Data Communications and Networking, McGraw Hill Edition */
+    Negative edge weights are found in various applications of graphs
+
+    Bellman–Ford algorithm can also detect negative cycles
+ */
+
+ /* Dijkstra's algorithm uses a priority queue to greedily select the closest 
+      vertex that has not yet been processed, and performs this relaxation 
+      process on all of its outgoing edges; 
+        
+    Relaxation: select the best(minimum in this case) 
+
+    By contrast, the Bellman–Ford algorithm simply relaxes all the edges, and does 
+      this | V | − 1 times, where | V | is the number of vertices in the graph
+    In each of these repetitions, the number of vertices with correctly calculated 
+      distances grows, from which it follows that eventually all vertices will 
+      have their correct distances.   
+ */
+
+ /* Run time - is it based on data structure used in implememtation
+
+    Bellman–Ford runs in O ( | V | ⋅ | E | ) time, where | V | and | E | are the 
+      number of vertices and edges respectively.
+      O ( | V | ⋅ | E | ) is for List based graph representation implementation
+ */
+
+ /* Bellman-Ford: algorithm calculates single source shortest path
+
+    Repeat the algorithm for all n vertices to get all source shortest path:
+
+    Step 1: initialize graph
+    for each vertex v in vertices:
+       distance[v] := inf      // Initially, all vertices have a weight of infinity
+       predecessor[v] := null  // And a null predecessor
+   
+    distance[source] := 0       // Except for the Source, where the Weight is zero 
+   
+    Step 2: relax edges repeatedly
+    for i from 1 to size(vertices)-1:
+        for each edge (u, v) with weight w in edges:
+            if distance[u] + w < distance[v]:
+                distance[v] := distance[u] + w
+                predecessor[v] := u
+
+    If the relax step is run once again and the cost from source to destination 
+      decreases, then the grpah has negative-weight cycle (Proof)
+ */
+
+ /* Application
+      For graph with Negative edge weights
+      Detect negative cycles and report their existence
+      Applications in routing
+      A distributed variant of the Bellman–Ford algorithm is used in distance-
+        vector routing protocols, Routing Information Protocol (RIP)
+ */
+
+ #include<stdio.h>
+
+  // Number of nodes
+  // Matrix representation of grpah, path known or unknown
+  // First intermediate vertex in  path from vertex u to v
+  // or hops or latency, depends on the network parameter
+  // index, to iterate through array
+
+ void readRoutingTable()
+  { 
+    // 999 to represent infinity: no direct edge, no known path between 
+    //   vertex u and v , and 0 if its same node
+    printf("\n If no direct edge between vertex u and v, or ");
+    printf("if cost is unknown, then enter 999, enter 0 if its same node");
+    printf("\n\n Enter the routing table : \n   |");
+
+            // From node
+              
+             // read cost/distance
+ 
+                       // save if edge/path exists
+  }
+
+ int main()
+  {    
+    readRoutingTable();    // read network graph in terms of adjacency matrix
+     
+              // assume via, through, or intermediate node
+
+             // If edge exists between vertex i and j, or 
+               //   path is known
+          
+              // Relax edges repeatedly, Check if j as 
+               // intermediate vertex is better, ij -> jk is better than ik
+              
+                  // update if i through j to k is better than existing i to k
+                 
+                    // update j as intermediate vertex
+                                        //   to go from i to k
+
+     // Print router tables
+       
+    return 0;
+
+  }
+
+ /* The algorithm was first proposed by Shimbel in 1955, ( but )
+      but is instead named after Richard Bellman and Lester Ford, Jr., who 
+      published it in 1958 and 1956, respectively.
+
+    Edward F. Moore also published the same algorithm in 1957, and for this reason 
+      it is also sometimes called the Bellman–Ford–Moore algorithm.
+ */
+
+ /* Like: 
+    GUI: Microsoft or Apple vs Xerox PARC
+    X-Ray: Edison or Röntgen
+           There are lot more on Edison or . .
+    Boson: Bose and Einstein or just Bose
+ */
+
+ /* Textbook: 
+    Behrouz Forouzon - Data Communications and Networking, McGraw Hill Edition 
+    Introduction to the design & analysis of algorithms / Anany Levitin
+    Introduction to Algorithms , Thomas H. Cormen , Charles E. Leiserson , 
+      Ronald L. Rivest, Clifford Stein (for Proof)
+    Data Structures and Algorithm Analysis in Java , Mark Allen Weiss
+ */
+
+ /* Output:
+     Enter number of nodes : 4
+
+     If no direct edge between vertex u and v, or if cost not known, then enter 999, enter 0 if its same node
+
+     Enter the routing table : 
+       | a b c d
+    ----------------------------
+     a | 0 5 1 4
+     b | 5 0 6 2
+     c | 1 6 0 3
+     d | 4 2 3 0
+
+     Table for router a
+    a:: 0 via a
+    b:: 5 via a
+    c:: 1 via a
+    d:: 4 via a
+
+     Table for router b
+    a:: 5 via b
+    b:: 0 via b
+    c:: 5 via d
+    d:: 2 via b
+
+     Table for router c
+    a:: 1 via c
+    b:: 5 via d
+    c:: 0 via c
+    d:: 3 via c
+    
+     Table for router d
+    a:: 4 via d
+    b:: 2 via d
+    c:: 3 via d
+    d:: 0 via d
+ */
+
--- Programs/RSA algorithm Encryption Decryption.c
+++ Programs/RSA algorithm Encryption Decryption.c
@@ -1,41 +1,49 @@
-/* 
-	Computer Networks Laboratory (Lab) 15CSL77
-	9. Write a program for simple RSA algorithm to encrypt and decrypt the data.
-*/
+ /* 
+    Computer Networks Laboratory (Lab) 15CSL77
+    9. Write a program for simple RSA algorithm to encrypt and decrypt the data.
+ */
 
-/*  RSA ( Ron Rivest, Adi Shamir, and Leonard Adleman) asymmetric Public Key cryptography algorithm
+ /* RSA ( Ron Rivest, Adi Shamir, and Leonard Adleman) asymmetric Public Key 
+      cryptography algorithm
     
     Asymmetric - Two keys, one key is public other private
 
     Cryptography - Encryption algorithm
+   
+    Cipher Text
 
     Based on the practical difficulty of the factorization of the product of 
-    two large prime numbers, the "factoring problem"
+      two large prime numbers, the "factoring problem"
 
-    Integer factorization is the decomposition of a composite number into a product of smaller integers
+    Integer factorization is the decomposition of a composite number into a 
+      product of smaller integers
     If only prime numbers are used, the process is called prime factorization
  
     Publish a public key based on two large prime numbers
 
-    Prime numbers must be kept secret. Anyone can use the public key to encrypt a message.
+    Prime numbers must be kept secret. 
+    Anyone can use the public key to encrypt a message. (Sender)
 
-    Only someone with knowledge of the prime numbers can decode the message feasibly
-*/
+    Only someone with knowledge of the prime numbers can decode the message 
+      feasibly (Receiver)
+ */
 
-/*  Generating Public Key
+ /* Generating Public Key
     1. Choose two distinct prime numbers, p, and q
     2. Compute n = p*q 
     3. Compute the totient of the product as λ(n) = (p − 1) * (q − 1)
        λ(n) can also be, λ(n) = lcm(λ(p), λ(q)) = lcm(p − 1, q − 1)
-    4. Choose any number 1 < e < λ(n) that is coprime to λ(n)
-       e
+    4. Choose any number 1 < e < λ(n) that is coprime to λ(n),        e
 
-    Coprime, two integers a and b are said to be relatively, mutually or co prime 
+    Coprime: two integers a and b are said to be relatively, mutually or co prime 
       if the only positive integer (factor) that divides both of them is 1
-    a, and b need not be prime. Example 14, 15
+    a, and b themself need not be prime. 
+    Example 14, 15 are co prime, but they themself are not prime, 
+      only common divisor is 1
 
     ( n , e )  is the Public key
 
+    Generating Private Key
     5. Compute d, the modular multiplicative inverse of e (mod λ(n)) 
        Inverse
        Multiplicative
@@ -43,114 +51,154 @@
        d * e mod λ(n) = 1 
     
     ( n , d )  is the Private key
-*/
+ */
 
-/* To encrypt, Cipher c, for message m
-   c = ( m power e ) mod n
-*/ 
+ /*To encrypt, message m, into cipher c
+   c = ( m power e ) mod n 
+   using public key (n, e) at sender 
+ */ 
 
-/* To decrypt, 
+ /*To decrypt, cipher text c to message m
    m = ( c power d ) mod n
-*/
+   using private key, (n, e) at receiver
+ */
 
+ /*Program should: 
+   Read prime numbers p, q
+   Read message, may be in an array
 
-/* For two numbers m, n 
-   lcm = m * n / gcd  
-   Computes gcd by Euclid’s algorithm
-*/
+   calculate n = p*q
 
-// ALGORITHM Euclid(m, n)
-//   Computes gcd(m, n) by Euclid’s algorithm
-//   Input: Two nonnegative, not-both-zero integers m and n
-//   Output: Greatest common divisor of m and n
-//   while n != 0 
-//     do
-//       r ← m mod n
-//       m ← n
-//       n ← r
-//     done 
-//   return m
+   calculate lambdaN = (p-1) * (q-1)  
 
-int euclidsAlgorithm(int m , int n )
- {
-   int r = 0;
+   find e, which is in between 1 and lambdaN and coprime to lambdaN
 
-   char temp; 
+   ( n, e ) will be Public key
+
+   encrypt message m, use number representation of character, by ( m power e )mod n
+   save as cipher text c, c = ( m power e ) mod n
    
-   printf("\n Euclid's Algorithm \n");
-   printf("\n r = %d \t m = %d \t n = %d \n", r, m, n);
+   To decrypt cipher text, use ( c power d ) mod n
+   m = ( c power d ) mod n
+   Then change number to its character representation
+ */
 
-   while( n!=0 )
-    {
-      r = m % n;
-      m = n;
-      n = r;
-      printf("\n r = %d \t m = %d \t n = %d \n", r, m, n);
-    }
+ /*Example:
+   p = 3,    q = 11
+   n = 33
 
-   printf("\n Algorithm complete \n GCD to be returned GCD = %d", m );
-   return m;
- }
+   λ(n) = (p − 1) * (q − 1) = (3-1)*(11-1) = 2*10 = 20
+   Choose any number 1 < e < λ(n) that is coprime to λ(n)
+   e = 3
 
-/*
-   Read p, q
+   ( n , e ), is the Public key =    ( 33, 3 )
+
+   Compute d, the modular multiplicative inverse of e (mod λ(n))
+   d such that, d * e mod λ(n) = 1
+   d * e mod λ(n) = d * 3 mod 20 = 7 * 3 mod 20 = 21 mod 20 = 1
    
-*/
+   d = 7   
 
-/* p = 13
-   q = 17
+   ( n , d ), is the Private key =   ( 33, 7 )
+ */
+
+ /*To encrypt, message m to Cipher text c
+   c = ( m power e ) mod n
+
+   Consider m = 2 3 4 , say its b c d, b is 2, c is 3 and d is 4
+   c = ( m power 3 ) mod 33
+
+   c = ( 2 power 3 ) mod 33 = 8
+   c = ( 3 power 3 ) mod 33 = 27
+   c = ( 4 power 3 ) mod 33 = 64 mod 33 = 31 
+
+   For message m = 2 3 4 , Cipher c = 8 27 31
+ */ 
+
+ /*To decrypt, cipher text c back to message m
+   m = ( c power d ) mod n
+
+   m = ( c power 7 ) mod 33
+
+   c = 8 27 31
+
+   m = (  8 power 7 ) mod 33 = 2
+   m = ( 27 power 7 ) mod 33 = 3
+   m = ( 31 power 7 ) mod 33 = 4
+
+   For Cipher c = 8 27 31 , message m = 2 3 4 
+ */
+
+ /*Example
+   p = 13,   q = 17
    n = p * q = 13 * 17 = 221
 
    λ(n) = ( p - 1 ) * ( q - 1 ) = 12*16 = 192
 
-   e, such that, 1 < e < λ(n) and coprime to λ(n)
+   e, such that, 1 < e < λ(n) and coprime to λ(n),    e = 35
 
-   e = 35
+   ( n , e ) is Public key = ( 221 , 35 )
 
-   ( n , e ) is Public key. ( 221 , 35 )
-
-   d * e mod λ(n) = 1
+   Private key, d, such that,    d * e mod λ(n) = 1
  
-   d * 35 mod 192 = 1
-
-   11 * 35 mod 192 =  1
+    d * 35 mod 192 = 1
+   11 * 35 mod 192 = 1
 
    ( n , d ) is Private key. ( 221 , 11 )  
-*/
+ */
 
-/* To encrypt, Cipher c, for message m
+ /*To encrypt, message m to Cipher c
    c = ( m power e ) mod n
-   Suppose, m = 123
+
+   Suppose, m = 1 2 3 , say its a b c, a is 1, b is 2 and c is 3
    c = ( m power 35 ) mod 221
 
-   c = 1 power 35 mod 221 = 1
-   c = 2 power 35 mod 221 = 59
-   c = 3 power 35 mode 221 = 61
+   c = ( 1 power 35 ) mod 221 = 1
+   c = ( 2 power 35 ) mod 221 = 59
+   c = ( 3 power 35 ) mod 221 = 61
 
-   c = 1 59 61
-*/ 
+   For message m = 1  2 3, the cipher c = 1 59 61
+ */ 
 
-/* To decrypt, 
-   m = ( c power d ) mod n
-
+ /*To decrypt, cipher c back to message m
+   m = ( c power  d ) mod n
    m = ( c power 11 ) mod 221
 
    c = 1 59 61
 
-   1 = ( 1 power 11 ) mod 221
-   2 = ( 59 power 11 ) mod 221
-   3 = ( 61 power 11 ) mod 221
+   m = (  1 power 11 ) mod 221 = 1
+   m = ( 59 power 11 ) mod 221 = 2
+   m = ( 61 power 11 ) mod 221 = 3
+
+   From cipher c = 1 59 61, message obtained, m = 1 2 3
+ */
+
+ // Select data type that is big enough to hold large integers
+ // Possibly, long int should suffice
+
+ int main()
+  { 
+
+    return 0;
+  }
+
+ /*
+   ALGORITHM Euclid(m, n)
+     Computes gcd(m, n) by Euclid’s algorithm
+     Input: Two nonnegative, not-both-zero integers m and n
+     Output: Greatest common divisor of m and n
+     while n != 0 
+       do
+         r ← m mod n
+         m ← n
+         n ← r
+       done 
+     return m
 */
 
-/* p = 3
-   q = 11
-   e = 3
-   d = 7
-   n = 33
-   
-
-*/
-
-/* Textbook: Behrouz Forouzon - Data Communications and Networking, McGraw Hill Edition 
-             Algorithms
-*/
+ /*Textbook: 
+   Behrouz Forouzon - Data Communications and Networking, McGraw Hill Edition 
+   Anany Levitin, Introduction to the design & analysis of algorithms
+   Thomas H. Cormen , Charles E. Leiserson , Ronald L. Rivest, Clifford Stein
+     Introduction to Algorithms
+ */
