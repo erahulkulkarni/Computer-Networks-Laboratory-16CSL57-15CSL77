f3590cc7c12f2b9cc5c5b50eb143d072506000e4
rahul <erahulkulkarni@gmail.com>
2018-08-30 06:21:06 +0530

    leakyBucket

--- Programs/Leaky bucket algorithm congestion control.c
+++ Programs/Leaky bucket algorithm congestion control.c
@@ -1,10 +1,83 @@
-/* 
-	Computer Networks Laboratory (Lab) 15CSL77
-	10. Write a program for congestion control using Leaky bucket algorithm.
-*/
+ /* 
+   Computer Networks Laboratory (Lab) 15CSL77
+   10. Write a program for congestion control using Leaky bucket algorithm.
+ */
 
-/*
+ /* Network layer
+    
+    Varialbe traffic rate
 
-*/
+    Match rate of output link/line
 
-/* Textbook: Behrouz Forouzon - Data Communications and Networking, McGraw Hill Edition */
+    Remove burstiness or jitter
+ */
+
+ /* MTU (Maximum Transmission Unit) for Ethernet is 1500 bytes.
+ */
+
+ /* The leaky bucket consists of a finite queue 
+
+    When a packet arrives, 
+      if there is room on the queue it is appended to the queue; 
+      otherwise it is discarded
+
+    At every clock tick one packet is transmitted (unless the queue is empty)
+ */
+
+ /*
+    Router A to B
+    A has queue so link to B does not get congested
+    A is restricting its output
+
+    A sends, fails, retries, instead match the capacity of A to B link
+
+    A limits to match B
+ */
+ 
+ /* Queue, enqueue, dequeue, isFull
+    Circular queue
+ */
+
+ /* Read parameters of the bucket:
+
+    1. Read Bucket(Queue) Size, bucketSize or queueSize
+       Size in terms of packets it can hold
+
+    2. Read output rate, bucketRate
+       In terms of packets Sent/output(dequeued) from bucket(queue)    
+
+
+    Read number of times to simulate, time
+
+    for t  ← 0 to time
+
+      Simulate flow into bucket, incoming traffic
+      
+      Read number of incoming packets, numberOfIncomingPackets
+
+      Read content of incoming packets, contentOfIncomingPacket[]
+
+      Now add to bucket
+
+      for packet p in numberOfIncomingPackets
+
+         if bucket(queue) not full
+           enqueue( contentOfIncomingPacket[p] )
+
+         else
+           packet p discarded
+
+      Adding to bucket complete, now simulate flow out of bucket
+      
+      Leaky bucket
+
+      for  t  ← 0 to bucketRate
+        outgoingPacket = dequeue( contentOfIncomingPacket )
+        print outgoingPacket
+    
+ */
+
+ /* Textbook: 
+    Behrouz Forouzon - Data Communications and Networking, McGraw Hill Edition 
+    Andrew S. Tanenbaum - Computer Networks
+ */
--- Programs/TCP IP Socket Client-Server.c
+++ Programs/TCP IP Socket Client-Server.c
@@ -1,39 +1,109 @@
-/* 
-	Computer Networks Laboratory (Lab) 15CSL77
-	8. Using TCP/IP sockets, write a client-server program to make client sending the file name 
-	and the server to send back the contents of the requested file if present.
-*/
+ /* 
+   Computer Networks Laboratory (Lab) 15CSL77
+   8. Using TCP/IP sockets, write a client-server program to make client sending 
+        the file name and the server to send back the contents of the requested 
+        file if present.
+ */
 
-/* Process-to-process delivery needs two identifiers, IP address and the port number, 
+ /*Process-to-process delivery needs two identifiers, IP address and the port number, 
    at each end to make a connection.   
 
-   IP to recognize computer on network, after which recognize process on the computer
-   using port number
+   IP to recognize computer on network, then recognize process on the computer
+     using port number
 
-   IP Address: uniquely defines a host on the Internet, logical addressing, hierarchical
+   IP Address: uniquely defines a host on the Internet, logical addressing, 
+     hierarchical
 
    Socket Addresses: combination of an IP address and a port number  
 
    Now turning to UNIX (every thing looks like a file!)
    Sockets can be used for interprocess communication
-   Socket is a type of file used for network communication between processes, network IPC
-   IPC Inter 
+   Socket is a type of file used for network communication between processes, 
+     network IPC, IPC Inter Process Communication
    Or for non-network communication between processes on a single host
 
    UNIX domain sockets are full duplex by default   
-*/
+ */
 
-/* Linux Programmer's Manual
+ /*Mechanisms of processes running on the same computer to communicate with 
+     one another: pipes, FIFOs, message queues, semaphores, and shared memory
+
+   Mechanisms that allow processes running on different computers (connected to 
+     a common network) to communicate with one another: network IPC
+
+   Socket network IPC interface, can be used by processes to communicate with 
+     other processes, regardless of where they are running: on the same machine 
+     or on different machines
+
+   Intermachine communication and Intramachine communication
+
+   TCP/IP protocol can be used to communicate
+ */
+
+ /*Socket interfaces: originally introduced in BSD in the early 1980s
+
+   socket is an abstraction of a communication endpoint
+
+   Like file descriptors used to access a file, socket descriptors used to access 
+     sockets
+
+   Functions that deal with file descriptors, such as read and write , will work 
+     with a socket descriptor
+
+   To create a socket, use the socket function
+
+     #include <sys/socket.h>
+     int socket(int domain, int type, int protocol);
+
+     Returns: file (socket) descriptor if OK, –1 on error
+     
+     domain:  nature of the communication, like AF_ (for address family)
+       AF_INET IPv4 Internet domain      
+
+     type: type of the socket, communication characteristics, like SOCK_STREAM
+       meaning sequenced, reliable, bidirectional, connection-oriented byte streams
+    
+     protocol: usually zero, selects default protocol for given domain, socket type
+
+     When done using the file descriptor, call close to relinquish access to the 
+       file or socket and free up the file descriptor for reuse
+ */
+
+ /*Addressing: machine's network address helps us identify the computer on the 
+     network we wish to contact, and the service helps us identify the particular 
+     process on the computer
+
+   Byte Ordering: big and little endian
+
+   API s for converting between the processor byte order and the network byte order
+ */
+
+ /*Address identifies a socket endpoint in a particular communication domain
+   IPv4 Internet domain ( AF_INET ), a socket address is represented by 
+     a sockaddr_in structure:
+
+     struct in_addr { in_addr_t     s_addr; };         // IPv4 address
+
+     struct sockaddr_in {
+                          sa_family_t     sin_family;  // address family
+			  in_port_t       sin_port;    // port number
+                          struct in_addr sin_addr;     // IPv4 address
+                        };
+ */
+
+
+ /*Linux Programmer's Manual
    man socket
    socket - create an endpoint for communication
 
    man getaddrinfo
 
-*/
+ */
 
 
 
 
-/* Textbook: Behrouz Forouzon - Data Communications and Networking, McGraw Hill Edition 
-   W. Richard Stevens, Advanced Programming in the UNIX Environment, Pearson Education
-*/
+ /*Textbook: W. Richard Stevens, Advanced Programming in the UNIX Environment, 
+     Pearson Education
+     RIP - Richard Stevens, also Rajeev Motwani
+ */
