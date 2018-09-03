727eb2cd79c6b16f6e2c4aa2bbb4a1f8ad91db4b
rahul <erahulkulkarni@gmail.com>
2018-09-03 16:26:01 +0530

    Leaky bucket

--- Programs/Leaky bucket algorithm congestion control.c
+++ Programs/Leaky bucket algorithm congestion control.c
@@ -117,46 +117,50 @@
     int k;
 
     printf("\n Assume maximum packets the bucket can hold is bucket size.");
-
     printf("\n Enter the bucket size : ");    scanf("%d", &bucketSize);
+
+    printf("\n Assume packets the bucket can let out every time unit is leak rate");
+    printf("\n Enter the leak rate : ");    scanf("%d", &leakRate);
 
     printf("\n Enter number of time instances to simulate : ");    scanf("%d", &time);
 
+    printf("\n Leaky bucket: ");
     for( i=0; i<time; i++ )
      {
-       printf("\n Enter the Number Of InComing Packets At Time T = %d : ", i); 
+       printf("\n\n Enter the Number Of InComing Packets At Time t = %d : ", i); 
        scanf("%d", &numberOfInComingPacketsAtTimeT);
 
        // If OpenMP (Open Multi-Processing) omp.h is used, and threading is enabled
        //     then, both enqueuing and dequeuing can be done simultaneously
        //     which resembles real life inflow and outflow of packets from bucket
 
-       printf("\n Enter the content of incoming packets at time T = %d", time); 
+       printf("\n   Enter the content of incoming packets at time t = %d\n", i); 
        for( j=0; j<numberOfInComingPacketsAtTimeT; j++ )        // Add to bucket
         {
          if( totalPacketsInBucket < bucketSize ) // If space in bucket, then
           { // Read the content and enqueue in bucket, content is just to 
-            printf("\n Enter content of packet: ");    // distinguish packets
+            printf("     Enter content of packet: ");    // distinguish packets
             scanf("%d", &bucket[++rear]);  // Add packet to rear of queue
             totalPacketsInBucket++;        // Increment number of packets in bucket
           }
          else
           { // totalPacketsInBucket == bucketSize, cannot add packet into bucket
-            printf("\n Bucket Overflow, drop the packet");
+            printf("\n     Bucket Overflow, drop the packet");
           }
         }
 
-       printf("\n Outgoing packets at time T = %d: ", i);
+       printf("\n\n   Outgoing packets at Time t = %d: ", i);
        for( j=0; j<leakRate; j++ ) // Remove leakRate number of packets from bucket
         {
          if( totalPacketsInBucket == 0 )
           {
-            printf("\n Bucket empty, underflow, no packets in bucket to let out");
+            printf("\n     Bucket empty, underflow, no packets in bucket to leak out");
           }
          else
-          { // Remove from front of bucket/queue
-            printf("\n Packet with content %d let out", bucket[front++]);
-          } // Increment front
+          { // Remove from front of bucket/queue; Increment front 
+            printf("\n     Packet with content %d leaked out", bucket[front++]);
+            totalPacketsInBucket--;    // and decrement number of packets in bucket
+          } 
         }
      }        
 
@@ -196,4 +200,44 @@
  /* Textbook: 
     Behrouz Forouzon - Data Communications and Networking, McGraw Hill Edition 
     Andrew S. Tanenbaum - Computer Networks
+ */
+
+ /* Output:
+    Assume maximum packets the bucket can hold is bucket size.
+
+    Enter the bucket size : 5
+
+    Assume packets the bucket can let out every time unit is leak rate
+    Enter the leak rate : 3
+
+    Enter number of time instances to simulate : 2
+
+    Leaky bucket: 
+
+    Enter the Number Of InComing Packets At Time t = 0 : 1
+
+      Enter the content of incoming packets at time t = 0
+        Enter content of packet: 101
+
+      Outgoing packets at Time t = 0: 
+        Packet with content 101 leaked out
+        Bucket empty, underflow, no packets in bucket to leak out
+        Bucket empty, underflow, no packets in bucket to leak out
+   
+
+    Enter the Number Of InComing Packets At Time t = 1 : 7
+
+      Enter the content of incoming packets at time t = 1
+        Enter content of packet: 102
+        Enter content of packet: 103
+        Enter content of packet: 104
+        Enter content of packet: 105
+        Enter content of packet: 106
+        Bucket Overflow, drop the packet
+        Bucket Overflow, drop the packet
+
+      Outgoing packets at Time t = 1: 
+        Packet with content 102 leaked out
+        Packet with content 103 leaked out
+        Packet with content 104 leaked out    
  */
